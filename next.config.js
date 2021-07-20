97ceed25c34161ebdad6f5ae989c110904b76ddc
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-19 21:07:56 -0500

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,44 @@
 module.exports = {
-  env: {
-    PUBLIC_URL: ''
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+
+  webpack5: true,
+
+  webpack: (config, { dev, isServer, webpack }) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next",
+            name: "static/media/[name].[hash].[ext]",
+          },
+        },
+      ],
+    })
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    })
+
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      exclude: /node_modules/,
+      loader: "esbuild-loader",
+      options: { loader: "tsx", target: "esnext" },
+    })
+
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: "react",
+      })
+    ) // Support JSX Transform per https://dev.to/rsa/speed-up-next-js-build-with-typescript-and-tailwind-css-418d
+
+    return config
   },
+
   experimental: {
     craCompat: true,
   },
-  // Remove this to leverage Next.js' static image handling
-  // read more here: https://nextjs.org/docs/api-reference/next/image
-  images: {
-    disableStaticImages: true
-  }  
 }
