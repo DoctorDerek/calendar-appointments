8cbfd8c16ba2035549b00a3bdb587ee4329f2257
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-21 00:27:09 -0500

    fix: add <StyledEngineProvider> to @/src/index.tsx to fix injection order in v4 -> v5 Material UI upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,11 @@ import React from "react"
 import { Provider } from "react-redux"
 import { createStore } from "redux"
 
-import { createTheme, ThemeProvider } from "@material-ui/core/styles"
+import {
+  createTheme,
+  StyledEngineProvider,
+  ThemeProvider,
+} from "@material-ui/core/styles"
 
 import AppContainer from "./components/App/AppContainer"
 import calendarApp from "./redux/reducers"
@@ -19,18 +23,27 @@ const store = createStore(
   calendarApp,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-
 export default function NextIndexWrapper() {
   // "Make sure to add a ThemeProvider at the root of your application,
   // as the defaultTheme is no longer available."
   // https://next.material-ui.com/guides/migration-v4/
   const defaultTheme = createTheme()
+  /**
+   * The style library used by default in v5 is emotion. While migrating from
+   * JSS to emotion, and if you are using JSS style overrides for your
+   * components (for example overrides created by makeStyles), you will need to
+   * take care of the CSS injection order. To do so, you need to have the
+   * StyledEngineProvider with the injectFirst option at the top of your
+   * component tree.
+   * https://next.material-ui.com/guides/migration-v4/#style-library */
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Provider store={store as any}>
-        <AppContainer />
-      </Provider>
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={defaultTheme}>
+        <Provider store={store as any}>
+          <AppContainer />
+        </Provider>
+      </ThemeProvider>
+    </StyledEngineProvider>
   )
 }
 
