2a4c4a2909240ef6f5a7a2f6222738831ae1b93b
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-21 00:30:14 -0500

    fix: update <title> in @/src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function MyApp({ Component, pageProps }: any) {
   return (
     <>
       <Head>
-        <title>React App</title>
+        <title>Calendar App - React / Redux - Material UI</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
