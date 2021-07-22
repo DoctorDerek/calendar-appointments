75c787a3223294266af751aef6e44508d915ce46
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-22 02:13:55 -0500

    chore: delete unused @/src/utils/serviceWorker.ts migrated from Create React App

--- src/components/NextIndexWrapper.tsx
+++ src/components/NextIndexWrapper.tsx
@@ -2,7 +2,6 @@ import { Provider } from "react-redux"
 
 import App from "@/src/components/App"
 import store from "@/src/redux/store"
-import * as serviceWorker from "@/src/utils/serviceWorker"
 import {
   createTheme,
   StyledEngineProvider,
@@ -32,8 +31,3 @@ export default function NextIndexWrapper() {
     </StyledEngineProvider>
   )
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
