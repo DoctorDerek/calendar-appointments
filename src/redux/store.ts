3e6cb3f8021ba3b9eeaddefda7ace7305422f1bf
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-22 02:14:40 -0500

    chore: fix "non-serializable" Redux data error by ignoring Date objects in @/src/redux/store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -14,7 +14,20 @@ const rootReducer: Reducer = (state: RootState, action: AnyAction) => {
 // A friendly abstraction over the standard Redux createStore function that
 // adds good defaults to the store setup for a better development experience.
 // https://redux-toolkit.js.org/api/configureStore
-const store = configureStore({ reducer: rootReducer })
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        // ignoredActions: ["your/action/type"],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ["agendaStatus.date", "dateObject.date"],
+        // Ignore these paths in the state
+        ignoredPaths: ["agendaStatus.date", "dateObject.date"],
+      },
+    }),
+})
 // Note: The Redux DevTools extension is now on by default in the Redux Toolkit.
 
 // Export the `RootState` and `AppDispatch` types from the Redux store itself
