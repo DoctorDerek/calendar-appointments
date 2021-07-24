02447f2e659e56f75be49d1b584e176060fe6abf
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-24 00:34:39 -0500

    feat: add <DateTimePicker> from @material-ui/lab (v5) to <AddReminder>

--- src/components/AddReminder.tsx
+++ src/components/AddReminder.tsx
@@ -1,5 +1,9 @@
+import { format } from "date-fns"
+import { useState } from "react"
+
 import { closeAddReminder } from "@/src/redux/actions"
 import { useAppDispatch, useAppSelector } from "@/src/redux/hooks"
+import { TextField } from "@material-ui/core"
 import Dialog from "@material-ui/core/Dialog"
 import DialogContent from "@material-ui/core/DialogContent"
 import DialogTitle from "@material-ui/core/DialogTitle"
@@ -8,7 +12,12 @@ import IconButton from "@material-ui/core/IconButton"
 import { Theme } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
 import CloseIcon from "@material-ui/icons/Close"
+import AdapterDateFns from "@material-ui/lab/AdapterDateFns"
+import DateTimePicker from "@material-ui/lab/DateTimePicker"
+import LocalizationProvider from "@material-ui/lab/LocalizationProvider"
 import { createStyles, WithStyles, withStyles } from "@material-ui/styles"
+
+const formatDateAndTime = (value: Date) => format(value, "MM/dd/yyyy hh:mm aaa")
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -24,7 +33,6 @@ const styles = (theme: Theme) =>
       top: "10px",
     },
   })
-
 const AddReminder = ({ classes }: WithStyles<typeof styles>) => {
   const { isOpen } = useAppSelector(
     ({ addReminderStatus }) => addReminderStatus
@@ -33,6 +41,9 @@ const AddReminder = ({ classes }: WithStyles<typeof styles>) => {
   const onClose = () => {
     dispatch(closeAddReminder())
   }
+  const [selectedDateTime, setSelectedDateTime] = useState<null | Date>(
+    new Date()
+  )
   return (
     <Dialog
       open={isOpen}
@@ -56,9 +67,22 @@ const AddReminder = ({ classes }: WithStyles<typeof styles>) => {
         <Typography>
           Use this space to create the UI to add a reminder to the calendar.
         </Typography>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DateTimePicker
+            value={selectedDateTime}
+            onChange={setSelectedDateTime}
+            getOpenDialogAriaText={(value) =>
+              `Choose date and time, selected date and time is ${
+                value && formatDateAndTime(value as Date)
+              }`
+            }
+            renderInput={(props) => (
+              <TextField {...props} helperText="selected time" />
+            )}
+          />
+        </LocalizationProvider>
       </DialogContent>
     </Dialog>
   )
 }
-
 export default withStyles(styles)(AddReminder)
