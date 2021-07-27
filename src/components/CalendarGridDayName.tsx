a090d5e2c0f622c5513b3f4ea65fb9090033cc52
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-26 23:48:31 -0500

    refactor: delete <CalendarGridDayName> by merging it into <CalendarGridDaysRow>

--- src/components/CalendarGridDayName.tsx
+++
@@ -1,7 +0,0 @@
-import Typography from "@material-ui/core/Typography"
-
-const CalendarGridDayName = ({ day }: { day: string }) => (
-  <Typography variant="h6">{day}</Typography>
-)
-
-export default CalendarGridDayName
--- src/components/CalendarGridDaysRow.tsx
+++ src/components/CalendarGridDaysRow.tsx
@@ -1,6 +1,6 @@
-import CalendarGridDayName from "@/src/components/CalendarGridDayName"
 import { daysArray } from "@/src/utils/dateUtils"
 import { Theme } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
 import { createStyles, WithStyles, withStyles } from "@material-ui/styles"
 
 const styles = (theme: Theme) =>
@@ -14,12 +14,17 @@ const styles = (theme: Theme) =>
     },
   })
 
-const CalendarGridDaysRow = ({ classes }: WithStyles<typeof styles>) => (
-  <div className={classes.daysRow}>
-    {daysArray.map((day) => (
-      <CalendarGridDayName key={day} day={day} />
-    ))}
-  </div>
-)
+function CalendarGridDaysRow({ classes }: WithStyles<typeof styles>) {
+  // daysArray is Sunday, Monday, ..., Saturday
+  return (
+    <div className={classes.daysRow}>
+      {daysArray.map((day) => (
+        <Typography variant="h6" key={day}>
+          {day}
+        </Typography>
+      ))}
+    </div>
+  )
+}
 
 export default withStyles(styles)(CalendarGridDaysRow)
