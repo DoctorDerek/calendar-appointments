0debaa577c495bbda2776eb2e4bd51054dd4f5cc
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-27 00:01:00 -0500

    refactor: use Tailwind CSS in <CalendarGridDaysRow>

--- src/components/CalendarGridDaysRow.tsx
+++ src/components/CalendarGridDaysRow.tsx
@@ -1,23 +1,10 @@
 import { daysArray } from "@/src/utils/dateUtils"
-import { Theme } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
-import { createStyles, WithStyles, withStyles } from "@material-ui/styles"
 
-const styles = (theme: Theme) =>
-  createStyles({
-    daysRow: {
-      display: "flex",
-      width: "100%",
-      flexBasis: "50px",
-      justifyContent: "space-evenly",
-      alignItems: "center",
-    },
-  })
-
-function CalendarGridDaysRow({ classes }: WithStyles<typeof styles>) {
+export default function CalendarGridDaysRow() {
   // daysArray is Sunday, Monday, ..., Saturday
   return (
-    <div className={classes.daysRow}>
+    <div className="flex items-center justify-between w-full">
       {daysArray.map((day) => (
         <Typography variant="h6" key={day}>
           {day}
@@ -26,5 +13,3 @@ function CalendarGridDaysRow({ classes }: WithStyles<typeof styles>) {
     </div>
   )
 }
-
-export default withStyles(styles)(CalendarGridDaysRow)
