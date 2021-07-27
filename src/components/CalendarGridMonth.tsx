cebdbc80da9455e18f2df9b15509d71fe1f68940
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-27 00:26:38 -0500

    refactor: use Tailwind CSS in <CalendarGridMonth>

--- src/components/CalendarGridMonth.tsx
+++ src/components/CalendarGridMonth.tsx
@@ -1,36 +1,23 @@
 import CalendarDay from "@/src/components/CalendarDay"
-import { Theme } from "@material-ui/core/styles"
-import { createStyles, WithStyles, withStyles } from "@material-ui/styles"
 
-const styles = (theme: Theme) =>
-  createStyles({
-    monthContainer: {
-      display: "flex",
-      width: "100%",
-      flexGrow: 1,
-      flexDirection: "row",
-      flexWrap: "wrap",
-      border: "1px solid lightgray",
-    },
-  })
-
-const CalendarGridMonth = ({
-  classes,
+export default function CalendarGridMonth({
   calendarCells,
   todaysDate,
-}: WithStyles<typeof styles> & {
+}: {
   calendarCells: DateObject[]
   todaysDate: Date
-}) => (
-  <div className={classes.monthContainer}>
-    {calendarCells.map((dateObject) => (
-      <CalendarDay
-        key={String(dateObject.date)}
-        todaysDate={todaysDate}
-        selectedDate={dateObject}
-      />
-    ))}
-  </div>
-)
-
-export default withStyles(styles)(CalendarGridMonth)
+}) {
+  return (
+    <div
+      className={"flex w-full flex-wrap border-1 border-solid border-gray-300"}
+    >
+      {calendarCells.map((dateObject) => (
+        <CalendarDay
+          key={String(dateObject.date)}
+          todaysDate={todaysDate}
+          selectedDate={dateObject}
+        />
+      ))}
+    </div>
+  )
+}
