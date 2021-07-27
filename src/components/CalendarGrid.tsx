314409913f16e305b4e835c2d5028752cc94271c
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-27 00:29:44 -0500

    refactor: improve code organization by merging <CalendarGridMonth> and <CalendarGridDaysRow> into <CalendarGrid>

--- src/components/CalendarGrid.tsx
+++ src/components/CalendarGrid.tsx
@@ -1,6 +1,6 @@
-import CalendarGridDaysRow from "@/src/components/CalendarGridDaysRow"
-import CalendarGridMonth from "@/src/components/CalendarGridMonth"
-import { getMonthCells } from "@/src/utils/dateUtils"
+import CalendarDay from "@/src/components/CalendarDay"
+import { daysArray, getMonthCells } from "@/src/utils/dateUtils"
+import Typography from "@material-ui/core/Typography"
 
 export default function CalendarGrid({ todaysDate }: { todaysDate: Date }) {
   const calendarCells = getMonthCells(todaysDate)
@@ -13,4 +13,41 @@ export default function CalendarGrid({ todaysDate }: { todaysDate: Date }) {
       />
     </div>
   )
+
+  function CalendarGridDaysRow() {
+    // daysArray is Sunday, Monday, ..., Saturday
+    return (
+      <div className="flex items-center justify-between w-full">
+        {daysArray.map((day) => (
+          <Typography className="text-3xl" key={day}>
+            {day}
+          </Typography>
+        ))}
+      </div>
+    )
+  }
+
+  function CalendarGridMonth({
+    calendarCells,
+    todaysDate,
+  }: {
+    calendarCells: DateObject[]
+    todaysDate: Date
+  }) {
+    return (
+      <div
+        className={
+          "flex w-full flex-wrap border-1 border-solid border-gray-300"
+        }
+      >
+        {calendarCells.map((dateObject) => (
+          <CalendarDay
+            key={String(dateObject.date)}
+            todaysDate={todaysDate}
+            selectedDate={dateObject}
+          />
+        ))}
+      </div>
+    )
+  }
 }
