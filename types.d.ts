81e996b806dfd416eec5699079dd2730eb0f0846
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-28 23:18:44 -0500

    chore: create Color type using HTML colors

--- types.d.ts
+++ types.d.ts
@@ -3,3 +3,14 @@ declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
   export default content
 }
+
+/** HTML colors for the color picker */
+type Color =
+  | "DodgerBlue"
+  | "Gray"
+  | "LightGray"
+  | "MediumSeaGreen"
+  | "Orange"
+  | "SlateBlue"
+  | "Tomato"
+  | "Violet"
