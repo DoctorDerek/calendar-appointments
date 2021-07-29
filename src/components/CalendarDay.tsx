895836bdbce231c68f33c259c3473c37ad07e50c
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-29 03:09:11 -0500

    feat: display reminders in correct time order in <CalendarDay> using an <Avatar> with a clock icon (<AccessAlarmIcon>) that dislpays the reminder on hover

--- src/components/CalendarDay.tsx
+++ src/components/CalendarDay.tsx
@@ -5,16 +5,22 @@ import {
   getMinutes,
   isSameDay,
   isSameMonth,
+  parseISO,
   setHours,
   setMinutes,
 } from "date-fns"
 import { useState } from "react"
 
 import { openAgenda } from "@/src/redux/agendaSlice"
-import { useAppDispatch } from "@/src/redux/hooks"
-import Avatar from "@material-ui/core/Avatar"
+import { useAppDispatch, useAppSelector } from "@/src/redux/hooks"
+import { Avatar } from "@material-ui/core"
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm"
 
 const classNames = (...classes: string[]) => classes.join(" ")
+// const formatDateAgenda = (date: Date) => format(date, "LLLL do, yyyy")
+// const formatDateAsDayOfWeek = (date: Date) => format(date, "EEEE")
+const formatDateCalendarDay = (date: Date) => format(date, "EEEE LLLL do, yyyy") // e.g. Thursday July 22, 2021
+const formatTimePicker = (date: Date) => format(date, "hh:mm aaa") // 08:00 am
 
 export default function CalendarDay({
   selectedDate,
@@ -23,27 +29,32 @@ export default function CalendarDay({
   selectedDate: Date
   todaysDate: Date
 }) {
-  // set selectedDate to current time from todaysDate
+  // set selectedDate to current time from todaysDate instead of 12:00AM
   selectedDate = setHours(selectedDate, getHours(todaysDate))
   selectedDate = setMinutes(selectedDate, getMinutes(todaysDate))
 
+  // configure the dispatch actions to open the agenda to the correct day
   const dispatch = useAppDispatch()
   const onDayClick = (selectedDate: Date) => {
     dispatch(openAgenda(selectedDate.toISOString()))
   }
   const [focused, setFocused] = useState(false)
-
   const onMouseOver = () => setFocused(true)
   const onMouseOut = () => setFocused(false)
   const onClick = () => onDayClick(selectedDate)
 
-  // const formatDateAgenda = (date: Date) => format(date, "LLLL do, yyyy")
-  // const formatDateAsDayOfWeek = (date: Date) => format(date, "EEEE")
-  const formatDateCalendarDay = (date: Date) =>
-    format(date, "EEEE LLLL do, yyyy") // e.g. Thursday July 22, 2021
-  const ariaLabel = formatDateCalendarDay(selectedDate)
+  // get the entire list of reminders from the Redux store
+  const { reminders } = useAppSelector(({ reminders }) => reminders)
+  // filter the reminders to only include those for the current day's agenda
+  const calendarDayReminders = reminders.filter((reminder) => {
+    return isSameDay(parseISO(reminder.dateISOString), selectedDate)
+  })
 
+  // determine isToday, which will be used to highlight today's date
   const isToday = isSameDay(selectedDate, todaysDate)
+
+  // generate the aria-label for the calendar day in long format
+  const ariaLabel = formatDateCalendarDay(selectedDate)
 
   return (
     <button
@@ -54,7 +65,7 @@ export default function CalendarDay({
       onClick={onClick}
       onKeyDown={(event) => event.key === "Enter" && onClick()}
       className={classNames(
-        "border-1 border-solid border-gray-300 cursor-pointer",
+        "border-1 border-solid border-gray-300 cursor-pointer flex flex-wrap justify-center items-center",
         isSameMonth(selectedDate, todaysDate)
           ? "bg-[rgba(255,255,255,0.4)]" // inside current month
           : "bg-gray-400" // outside current month
@@ -63,11 +74,11 @@ export default function CalendarDay({
     >
       <Avatar
         className={classNames(
-          "text-lg text-black mx-auto border-transparent border-1 border-solid",
+          "text-black border-transparent border-1 border-solid",
           isToday && focused
-            ? "bg-purple-600 shadow-xl border-current" // focused today's avatar
+            ? "bg-purple-600 shadow-xl border-current m-[1px] md:mx-0.5" // focused today's avatar
             : isToday
-            ? "bg-purple-400 shadow-xl border-current" // today's avatar
+            ? "bg-purple-400 shadow-xl border-current m-[1px] md:mx-0.5" // today's avatar
             : focused
             ? "bg-gray-400 shadow-xl border-current" // focused avatar for normal date
             : "bg-transparent" // regular avatar for normal date
@@ -76,7 +87,22 @@ export default function CalendarDay({
       >
         {getDate(selectedDate)}
       </Avatar>
-      {/* reminders go here */}
+      {calendarDayReminders.map(({ id, dateISOString, color, text }) => (
+        <div className="group" key={id}>
+          <Avatar
+            style={{ backgroundColor: color }}
+            className="w-5 h-5 m-[1px] md:mx-0.5 border-1 border-solid border-gray-300"
+          >
+            <AccessAlarmIcon className="w-4 h-4" />
+          </Avatar>
+          <div
+            className="absolute z-20 hidden p-2 text-xl transition-opacity duration-500 shadow-lg group-hover:block rounded-3xl"
+            style={{ backgroundColor: color }}
+          >
+            {formatTimePicker(parseISO(dateISOString))} {text}
+          </div>
+        </div>
+      ))}
     </button>
   )
 }
