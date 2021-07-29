d0fb01627d1a30d61df66a150365070f2f479245
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-29 00:34:57 -0500

    feat: add "frosted glass" background effect using an Unsplash image by Benjamin Patin

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import { addMonths, format, subMonths } from "date-fns"
+import Image from "next/image"
 import { useState } from "react"
 
 import AddReminder from "@/src/components/AddReminder"
@@ -12,7 +13,6 @@ import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft"
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight"
 
 const formatDateAsMonthApp = (date: Date) => format(date, "LLLL yyyy")
-
 export default function App() {
   const [todaysDate, setTodaysDate] = useState(new Date())
 
@@ -25,32 +25,46 @@ export default function App() {
   }
 
   return (
-    <div className="flex items-center justify-center w-full">
-      <Paper className="flex flex-col items-center justify-center w-full h-full p-3 m-6">
-        <header className="flex items-center justify-between w-full my-10">
-          <IconButton
-            aria-label="Previous Month"
-            className="w-16 h-16 mb-4 ml-4 text-blue-500 transition-all duration-500 bg-blue-100 border-blue-300 border-solid fill-current right-2 top-2 border-1 hover:bg-blue-300 hover:text-blue-700 hover:border-blue-500"
-            onClick={prevMonth}
-          >
-            <KeyboardArrowLeftIcon className="w-12 h-12" />
-          </IconButton>
-          <Typography className="text-6xl font-bold">
-            {formatDateAsMonthApp(todaysDate)}
-          </Typography>
-          <IconButton
-            aria-label="Next Month"
-            className="w-16 h-16 mb-4 text-blue-500 transition-all duration-500 bg-blue-100 border-blue-300 border-solid fill-current right-2 top-2 border-1 hover:bg-blue-300 hover:text-blue-700 hover:border-blue-500"
-            onClick={nextMonth}
-          >
-            <KeyboardArrowRightIcon fontSize="large" className="w-12 h-12" />
-          </IconButton>
-        </header>
-        <CalendarGrid todaysDate={todaysDate} />
-        <AddReminderFab />
-      </Paper>
-      <AgendaDay />
-      <AddReminder />
-    </div>
+    <>
+      <div className="relative z-10 flex items-center justify-center w-full">
+        <Paper
+          className="flex flex-col items-center justify-center w-full h-full p-3 m-6 rounded-3xl"
+          classes={{
+            root: "backdrop-filter backdrop-grayscale backdrop-blur bg-[rgba(255,255,255,0.3)]",
+          }}
+        >
+          <header className="flex items-center justify-between w-full my-10">
+            <IconButton
+              aria-label="Previous Month"
+              className="w-16 h-16 mb-4 ml-4 text-blue-500 transition-all duration-500 bg-blue-100 border-blue-300 border-solid fill-current right-2 top-2 border-1 hover:bg-blue-300 hover:text-blue-700 hover:border-blue-500"
+              onClick={prevMonth}
+            >
+              <KeyboardArrowLeftIcon className="w-12 h-12" />
+            </IconButton>
+            <Typography className="text-6xl font-bold">
+              {formatDateAsMonthApp(todaysDate)}
+            </Typography>
+            <IconButton
+              aria-label="Next Month"
+              className="w-16 h-16 mb-4 text-blue-500 transition-all duration-500 bg-blue-100 border-blue-300 border-solid fill-current right-2 top-2 border-1 hover:bg-blue-300 hover:text-blue-700 hover:border-blue-500"
+              onClick={nextMonth}
+            >
+              <KeyboardArrowRightIcon fontSize="large" className="w-12 h-12" />
+            </IconButton>
+          </header>
+          <CalendarGrid todaysDate={todaysDate} />
+          <AddReminderFab />
+        </Paper>
+        <AgendaDay />
+        <AddReminder />
+      </div>
+      <div className="absolute inset-0 z-0 w-screen h-screen">
+        <Image
+          src="/benjamin-patin-dOzoyaYjCbM-unsplash.jpg"
+          alt="Ocean waves breaking by Benjamin Patin on Unsplash"
+          layout="fill"
+        />
+      </div>
+    </>
   )
 }
